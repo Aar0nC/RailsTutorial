7f15fc3199566b82623d9bb01d26c6d088b160e1
Aaron Cotter <acotter@tfs.ca>
2017-08-17 11:41:14 -0400

    Finish static pages and tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'application#hello'
+  get 'static_pages/contact'
+  root 'static_pages#home'
 end
