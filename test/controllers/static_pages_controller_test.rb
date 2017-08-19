0737b80195fced84999de6b2df6a40ac568a6e9f
Aaron Cotter <acotter@tfs.ca>
2017-08-19 11:44:43 -0400

    Simplify routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   test "get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
     assert_select "title", @base_title
   end
 
   test "get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
