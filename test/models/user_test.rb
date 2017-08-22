01c539f94efa054072eb9bec9065aaae064d2541
Aaron Cotter <acotter@tfs.ca>
2017-08-21 21:03:50 -0400

    Add user validation and validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Aaron Cotter", email: "aaron@example.com")
+  end
+
+  test "valid user" do
+    assert @user.valid?, "User isn't valid"
+  end
+
+  test "name is present" do
+    @user.name = " "
+    assert_not @user.valid?, "Name must be present"
+  end
+
+  test "email is present" do
+    @user.email = " "
+    assert_not @user.valid?, "Email must be present"
+  end
+
+  test "name is at most 50 characters" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email is at most 255 characters" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
