fb37ad87b7e4751b2618ace468b804e1395f0cda
Aaron Cotter <acotter@tfs.ca>
2017-08-21 21:17:52 -0400

    Implement email validation and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,22 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "accept valid email addresses" do
+    valid_addresses = %w[aaron@example.com USER@foo.COM first.last@foo.jp
+alice+bob@bob.cp A_US-ER@foo.bar]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} is valid"
+    end
+  end
+
+  test "reject invalid email addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_addresses.inspect} is invalid"
+    end
+  end
 end
