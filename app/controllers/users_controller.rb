d8a388d15663fb46af2c89cff28d29584acff5c5
Aaron Cotter <acotter@tfs.ca>
2017-08-24 12:42:23 -0400

    add new user sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user].permit(:name, :email, :password, :password_confirmation))
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 end
