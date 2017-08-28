d0429875044af234d2dad7c0960d1637e0254d45
Aaron Cotter <acotter@tfs.ca>
2017-08-28 19:09:17 -0400

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
   resources :users
 end
